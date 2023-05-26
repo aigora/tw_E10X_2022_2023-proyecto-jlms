bb76975d7d1b98c184dfcdfef4d0f743d7743d28
matiaspolo <79769750+matiaspolo@users.noreply.github.com>
2023-05-26 22:52:34 +0200

    Update del menú del usuario

--- proyectojlms/main.c
+++ proyectojlms/main.c
@@ -11,15 +11,18 @@ int main()
   mes data[10]; //tenemos 10 meses de datos, luego se define vector de estructuras
   char accion[20],accion_2[20],aux[100]; //variables auxiliares que recogeran las acciones del usuario y recorreran el archivo
   //car es una variable auxiliar y el resto son las instrucciones que guiaran el programa
-  char car,basura, open[] = "abrir",length[] = "contar",end[] = "finalizar",all[] = "todos",elegir[] = "elegir",grafics[]="graficas";
+  char car,basura,begin[] = "comenzar",consultar[] = "datos",end[] = "finalizar",all[] = "todos",elegir[] = "elegir",grafics[]="graficas";
   pf = fopen("generacion.txt", "r"); //puntero dirigido abrir el fichero en modo lectura
   printf("Bienvenido al programa\n");
-  printf("Se recomienda para un correcto funcionaminto de las graficas la pantalla completa\n");
-  printf("A continuacion, eliga la acccion que quiere realizar, diga: 'abrir'\n");//primera instruccion
+  printf("Se recomienda para un correcto funcionaminto de las graficas la pantalla completa y el uso de minusculas\n");
+  printf("Recuerda que en cualquier momento, si escribes 'finalizar', se acaba el programa\n");
+  printf("A continuacion, para abrir el fichero, diga : comenzar\n");//primera instruccion
   scanf("%9[^\n]",accion);
   scanf("%c",&basura);
   guardian_2(accion,1);//revision de instruccion, perro guardian
-  if(strcmp(accion,open) == 0)//el usuario ha tecleado 'abrir' correctamente
+  if(strcmp(accion,end) == 0)
+    return 0;
+  if(strcmp(accion,begin) == 0)//el usuario ha tecleado 'abrir' correctamente
   {
     if (pf == NULL) // Si el resultado es NULL mensaje de error
         {
@@ -28,19 +31,19 @@ int main()
         }
     else
         {
-            limpia_consola();
-            printf("el fichero ha sido abierto correctamente\n");
-            printf("ahora que accion quieres realizar? Puedes 'finalizar' el programa o 'contar' las lineas\n");
-            scanf("%9[^\n]",accion);
-            scanf("%c",&basura);
-            guardian_2(accion,2);//revision de instruccion, perro guardian
-            if(strcmp(accion,end) == 0)//se finaliza el programa
+        limpia_consola();
+        printf("El fichero ha sido abierto correctamente\n");
+        printf("Ahora que accion quieres realizar? Puedes ver los 'datos' del fichero o 'ingresar' nuevos datos\n");
+        scanf("%9[^\n]",accion);
+        scanf("%c",&basura);
+        guardian_2(accion,2);//revision de instruccion, perro guardian
+        if(strcmp(accion,end) == 0)//se finaliza el programa
             {
                 printf("fin\n");
                 fclose(pf);
                 return 0;
             }
-            if(strcmp(accion,length) == 0)//se elige contar el numero de lineas
+        if(strcmp(accion,consultar) == 0)//se elige contar el numero de lineas
             {
                limpia_consola();
                while (fscanf(pf, "%c", &car) != EOF)//recorrido de cada caracter del fichero
@@ -265,113 +268,113 @@ int main()
                     }//fin de lectura de ultima fila
                 }//cierre de bluce que detecta el salto de linea
                 }//cierre de bucle que lee cada caracter hasta EOF
-            }//fin de la orden de contar las filas
-        fseek(pf, 0, SEEK_END);//se situa el puntero en el inicio y recorre el fichero hasta el final
-        fsize = ftell(pf);//valor del numero de bytes
-        printf("Informacion sobre el fichero:\n");
-        printf("Numero de bytes: %li\n",fsize);
-        printf("Numero de lineas: %d\n",nLineas);
-        printf("Ahora puedes elegir lo que quieres hacer\n");//segunda instruccion del usuario
-        printf("Puedes mostrar o 'todos' los datos o 'elegir' los meses que deseas estudiar\n");
-        printf("Ver graficas (escribe 'graficas')\n");
-        scanf("%9[^\n]",accion);//se lee su instruccion hasta detectar salto de linea
-        scanf("%c",&basura);
-        guardian_2(accion,3);//revision de instruccion, perro guardian
-        limpia_consola();
-        if(strcmp(accion,all) == 0)//orden de mostrar todos los datos
-        {
-            limpia_consola();
-            printf("has seleccionado mostrar todos los datos, en ese caso seria:\n");
-            for(i=0;i<10;i++)//el bucle for recorre los datos de los 10 meses
-            {
-            printf("%d/%d:\n",data[i].date.month,data[i].date.year);
-            printf("hidraulica: %f\n",data[i].hidraulica);
-            printf("turbbombeo: %f\n",data[i].turbbombeo);
-            printf("nuclear: %f\n",data[i].nuclear);
-            printf("carbon: %f\n",data[i].carbon);
-            printf("fuelgas: %f\n",data[i].fuelgas);
-            printf("motdiesel: %f\n",data[i].motdiesel);
-            printf("turbinagas: %f\n",data[i].turbinagas);
-            printf("turbvapor: %f\n",data[i].turbvapor);
-            printf("ccombinado: %f\n",data[i].ccombinado);
-            printf("hidroeolica: %f\n",data[i].hidroeolica);
-            printf("eolica: %f\n",data[i].eolica);
-            printf("solarfoto: %f\n",data[i].solarfoto);
-            printf("solarterm: %f\n",data[i].solarterm);
-            printf("otrasreno: %f\n",data[i].otrasreno);
-            printf("cogenerac: %f\n",data[i].cogenerac);
-            printf("norenov: %f\n",data[i].norenov);
-            printf("residrenov: %f\n",data[i].residrenov);
-            printf("genertotal: %f\n",data[i].genertotal);
-            printf("\n");
-            }
-        }//cierre del caso para mostrar todos los datos
-        if(strcmp(accion,elegir) == 0)//el usario quiere solo mostrar un mes
-        {
-            limpia_consola();
-            printf("de acuerdo, te voy a mostrar los datos de un solo mes, escribelo:\n");
-            scanf("%15[^\n]",accion);
+            fseek(pf, 0, SEEK_END);//se situa el puntero en el inicio y recorre el fichero hasta el final
+            fsize = ftell(pf);//valor del numero de bytes
+            fclose(pf);
+            printf("Informacion sobre el fichero:\n");
+            printf("Numero de bytes: %li\n",fsize);
+            printf("Numero de lineas: %d\n",nLineas);
+            //segunda instruccion del usuario
+            printf("Ahora puedes mostrar o 'todos' los datos o 'elegir' los meses que deseas estudiar\n");
+            printf("Ver graficas (escribe 'graficas')\n");
+            scanf("%9[^\n]",accion);//se lee su instruccion hasta detectar salto de linea
             scanf("%c",&basura);
-            guardian_2(accion,4);//comprabacion de instruccion del usuario
-            num = elige_un_mes(accion); //funcion que devuelve el valor apto para cada mes
-            for(i=num-1;i<num;i++)//sea cual sea el valor del numero, solo se avanza una posicion
+            guardian_2(accion,3);//revision de instruccion, perro guardian
+            limpia_consola();
+            if(strcmp(accion,end) == 0)
+                return 0;
+            if(strcmp(accion,all) == 0)//orden de mostrar todos los datos
                 {
-                printf("%d/%d: \n",data[i].date.month,data[i].date.year);
-                printf("hidraulica: %f\n",data[i].hidraulica);
-                printf("turbbombeo: %f\n",data[i].turbbombeo);
-                printf("nuclear: %f\n",data[i].nuclear);
-                printf("carbon: %f\n",data[i].carbon);
-                printf("fuelgas: %f\n",data[i].fuelgas);
-                printf("motdiesel: %f\n",data[i].motdiesel);
-                printf("turbinagas: %f\n",data[i].turbinagas);
-                printf("turbvapor: %f\n",data[i].turbvapor);
-                printf("ccombinado: %f\n",data[i].ccombinado);
-                printf("hidroeolica: %f\n",data[i].hidroeolica);
-                printf("eolica: %f\n",data[i].eolica);
-                printf("solarfoto: %f\n",data[i].solarfoto);
-                printf("solarterm: %f\n",data[i].solarterm);
-                printf("otrasreno: %f\n",data[i].otrasreno);
-                printf("cogenerac: %f\n",data[i].cogenerac);
-                printf("norenov: %f\n",data[i].norenov);
-                printf("residrenov: %f\n",data[i].residrenov);
-                printf("genertotal: %f\n",data[i].genertotal);
-                }//cierre de mostrar el mes
-        }//cierre de caso elegir un mes
-
-        if(comparar_cadenas(accion,grafics) == 1)
-        {
-            while(comparar_cadenas(accion,"finalizar")==0)
-            {
-                printf("Desde que mes deseas estudiar (como maximo octubre)(escribir ej:'marzo')\n");
-                scanf("%15[^\n]",accion);//se lee su instruccion hasta detectar salto de linea
-                scanf("%c",&basura);
-                guardian_2(accion,4);
-                mes_1=enumerar_meses(accion);
-                printf("Hasta que mes deseas estudiar(como maximo octubre)(escribir ej:'febrero')\n");
-                scanf("%15[^\n]",accion_2);//se lee su instruccion hasta detectar salto de linea
-                scanf("%c",&basura);
-                guardian_2(accion_2,4);
                 limpia_consola();
-                mes_2=enumerar_meses(accion_2);
-                if(mes_2<mes_1)
+                printf("Has seleccionado mostrar todos los datos, en ese caso seria:\n");
+                for(i=0;i<10;i++)//el bucle for recorre los datos de los 10 meses
+                    {
+                    printf("%d/%d:\n",data[i].date.month,data[i].date.year);
+                    printf("hidraulica: %f\n",data[i].hidraulica);
+                    printf("turbbombeo: %f\n",data[i].turbbombeo);
+                    printf("nuclear: %f\n",data[i].nuclear);
+                    printf("carbon: %f\n",data[i].carbon);
+                    printf("fuelgas: %f\n",data[i].fuelgas);
+                    printf("motdiesel: %f\n",data[i].motdiesel);
+                    printf("turbinagas: %f\n",data[i].turbinagas);
+                    printf("turbvapor: %f\n",data[i].turbvapor);
+                    printf("ccombinado: %f\n",data[i].ccombinado);
+                    printf("hidroeolica: %f\n",data[i].hidroeolica);
+                    printf("eolica: %f\n",data[i].eolica);
+                    printf("solarfoto: %f\n",data[i].solarfoto);
+                    printf("solarterm: %f\n",data[i].solarterm);
+                    printf("otrasreno: %f\n",data[i].otrasreno);
+                    printf("cogenerac: %f\n",data[i].cogenerac);
+                    printf("norenov: %f\n",data[i].norenov);
+                    printf("residrenov: %f\n",data[i].residrenov);
+                    printf("genertotal: %f\n",data[i].genertotal);
+                    printf("\n");
+                    }
+                }//cierre del caso para mostrar todos los datos
+
+            if(strcmp(accion,elegir) == 0)//el usario quiere solo mostrar un mes
                 {
-                    i=mes_1;
-                    mes_1=mes_2;
-                    mes_2=i;
-                }
-                grafica(data,mes_1,mes_2);
-                printf("\n\n-Atras\n-Finalizar\n");
+                limpia_consola();
+                printf("De acuerdo, te voy a mostrar los datos de un solo mes, escribelo:\n");
                 scanf("%15[^\n]",accion);
-                guardian_2(accion,6);
-                limpia_consola();
-            }
-        }
+                scanf("%c",&basura);
+                guardian_2(accion,4);//comprabacion de instruccion del usuario
+                num = elige_un_mes(accion); //funcion que devuelve el valor apto para cada mes
+                for(i=num-1;i<num;i++)//sea cual sea el valor del numero, solo se avanza una posicion
+                    {
+                    printf("%d/%d: \n",data[i].date.month,data[i].date.year);
+                    printf("hidraulica: %f\n",data[i].hidraulica);
+                    printf("turbbombeo: %f\n",data[i].turbbombeo);
+                    printf("nuclear: %f\n",data[i].nuclear);
+                    printf("carbon: %f\n",data[i].carbon);
+                    printf("fuelgas: %f\n",data[i].fuelgas);
+                    printf("motdiesel: %f\n",data[i].motdiesel);
+                    printf("turbinagas: %f\n",data[i].turbinagas);
+                    printf("turbvapor: %f\n",data[i].turbvapor);
+                    printf("ccombinado: %f\n",data[i].ccombinado);
+                    printf("hidroeolica: %f\n",data[i].hidroeolica);
+                    printf("eolica: %f\n",data[i].eolica);
+                    printf("solarfoto: %f\n",data[i].solarfoto);
+                    printf("solarterm: %f\n",data[i].solarterm);
+                    printf("otrasreno: %f\n",data[i].otrasreno);
+                    printf("cogenerac: %f\n",data[i].cogenerac);
+                    printf("norenov: %f\n",data[i].norenov);
+                    printf("residrenov: %f\n",data[i].residrenov);
+                    printf("genertotal: %f\n",data[i].genertotal);
+                    }//cierre de mostrar el mes
+                }//cierre de caso elegir un mes
 
+            if(comparar_cadenas(accion,grafics) == 1)
+                {
+                while(comparar_cadenas(accion,"finalizar")==0)
+                {
+                    printf("Desde que mes deseas estudiar (como maximo octubre)(escribir ej:'marzo')\n");
+                    scanf("%15[^\n]",accion);//se lee su instruccion hasta detectar salto de linea
+                    scanf("%c",&basura);
+                    guardian_2(accion,4);
+                    mes_1=enumerar_meses(accion);
+                    printf("Hasta que mes deseas estudiar(como maximo octubre)(escribir ej:'febrero')\n");
+                    scanf("%15[^\n]",accion_2);//se lee su instruccion hasta detectar salto de linea
+                    scanf("%c",&basura);
+                    guardian_2(accion_2,4);
+                    limpia_consola();
+                    mes_2=enumerar_meses(accion_2);
+                    if(mes_2<mes_1)
+                    {
+                        i=mes_1;
+                        mes_1=mes_2;
+                        mes_2=i;
+                    }
+                    grafica(data,mes_1,mes_2);
+                    printf("\n\n-Atras\n-Finalizar\n");
+                    scanf("%15[^\n]",accion);
+                    guardian_2(accion,6);
+                    limpia_consola();
+                }
+                }
+            }//fin de la orden de consultar datos
         return 0;
         }//cierre del caso en el que el fichero se ha abierto correctamente
-  }//cierre del caso donde el usuario ha tecleado 'abrir'
-  else
-    printf("error, se finaliza el programa\n");
-return 0;
+  }//cierre del caso donde el usuario ha tecleado 'comezar'
 }
 
